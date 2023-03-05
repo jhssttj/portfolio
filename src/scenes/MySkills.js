8e81a3b0cb04a86121fdef4f4e74406cbe32a22c
Kevin Lee <jhssttj@hotmail.com>
2023-03-04 18:43:40 -0700

    Add innovative section to skills

--- src/scenes/MySkills.js
+++ src/scenes/MySkills.js
@@ -81,6 +81,31 @@ const MySkills = () => {
 
         </motion.div>
 
+        {/*INNOVATIVE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{once:true, amount:0.5}}
+          transition={{delay:0.2, duration:0.5}}
+          variants={{
+            hidden: {opacity: 0, y: 50},
+            visible: {opacity: 1, y: 0}
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-platfair font-semibold text-5xl">02</p>
+              <p className="font-platfair font-semibold text-3xl mt-3">Innovative</p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]"/>
+          </div>
+          <p className="mt-5">
+            Random text goes here
+          </p>
+
+        </motion.div>
+
       </div>
 
     </section>
