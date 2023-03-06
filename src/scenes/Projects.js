7f5e5d17381273f052897b6fe6de7515f1b41270
Kevin Lee <jhssttj@hotmail.com>
2023-03-06 16:22:49 -0700

    Add div span for each project slots

--- src/scenes/Projects.js
+++ src/scenes/Projects.js
@@ -1,6 +1,26 @@
 import LineGradient from '../components/LineGradient';
 import {motion} from "framer-motion";
 
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {staggerChildren: 0.2}
+  }
+}
+
+const projectVaraint = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1}
+}
+
+const Project = ({title}) => {
+  const projectTitle = title.split(" ").join("-").toLowerCase();
+  return (
+    <motion.div></motion.div>
+  )
+}
+
 const Projects = () => {
   return (
     <section id="projects" className="pt-48 pb-48">
@@ -20,13 +40,33 @@ const Projects = () => {
           My <span className="text-red">PRO</span>JECTS
         </p>
         <div className="flex justify-center mt-5">
-        <LineGradient width="w-1/3" />
+          <LineGradient width="w-1/3" />
         </div>
       </div>
       <p className="mt-10 mb-10">
         My projects goes here
       </p>
     </motion.div>
+
+    {/*Projects */}
+    <div className="flex justify-center">
+      <motion.div
+        className="sm:grid sm:grid-cols-3"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{once:true, amount:0.5}}
+        transition={{duration:0.5}}
+        variants={container}
+      >
+        {/*Row 1 */}
+        <div
+          className="flex justify-center text-center items-center p-10 bg-red max-w-[400px] max-h[400px] text-2xl font-playfair font-semibold"
+        >
+          BEAUTFIUL U
+          <Project title="project 1"></Project>
+
+      </motion.div>
+    </div>
     </section>
   )
 }
