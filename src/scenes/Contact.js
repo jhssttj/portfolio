5b69741da6f6d751ddcdfa6cdb65022de6ed9f1f
Kevin Lee <jhssttj@hotmail.com>
2023-03-22 18:33:06 -0600

    Add form and input with error validations

--- src/scenes/Contact.js
+++ src/scenes/Contact.js
@@ -25,8 +25,8 @@ const Contact = () => {
           viewport={{once:true, amount:0.5}}
           transition={{duration:0.5}}
           variants={{
-            hidden: {opacity: 0, scale: 0.8},
-            visible: {opacity: 1, scale: 1}
+            hidden: {opacity: 0, x:-50},
+            visible: {opacity: 1, x:0}
           }}
          >
           <div>
@@ -41,7 +41,51 @@ const Contact = () => {
 
         {/*Form and Image  */}
         <div className="md:flex md:justify-between gap-16 mt-5">
-          <motion.div>
+          <motion.div
+          className="basis-1/2 flex justify-center"
+             initial="hidden"
+             whileInView="visible"
+             viewport={{once:true, amount:0.5}}
+             transition={{duration:0.5}}
+             variants={{
+               hidden: {opacity: 0, x:50},
+               visible: {opacity: 1, y:0}
+             }}>
+            <img src="../assets/contact-image.jpeg" alt="contact"/>
+          </motion.div>
+          <motion.div
+           className="basis-1/2 mt-10 md:mt-0"
+           initial="hidden"
+           whileInView="visible"
+           viewport={{once:true, amount:0.5}}
+           transition={{delay: 0.2, duration:0.5}}
+           variants={{
+             hidden: {opacity: 0, x:50},
+             visible: {opacity: 1, y:0}
+           }}>
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              action=""
+              method="POST"            
+            >
+              <input
+                className="w-full bg-blue font-semibold placeholder-opaque-black p-3"
+                type="text"
+                placeholder="NAME"
+                {...register("name",{
+                  required: true,
+                  maxLength: 100
+                })}
+              />
+              {errors.name && (
+                <p className="text-red mt-1">
+                  {errors.name.type === 'required' && 'This field is required.'}
+                  {errors.name.type === 'maxLength' && 'Max Length of 100 exceeded.'}
+                </p>
+              )}
+
+            </form>
             
           </motion.div>
 
