544b51d95b5827ffb2b1a3cfbb2252dbd720d87b
Kevin Lee <jhssttj@hotmail.com>
2023-04-03 19:09:45 -0600

    Fix nav bar positioning when in small media mode

--- src/scenes/Navbar.js
+++ src/scenes/Navbar.js
@@ -19,10 +19,11 @@ const Link = ({ page, selectedPage, setSelectedPage}) => {
 const Navbar = ({ selectedPage, setSelectedPage}) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
   const isAboveSmallScreens = useMediaQuery("(min-width: 768px");
+  const navBarIconClass = (isAboveSmallScreens? "justify-center":"justify-end")
 
   return (
-    <nav className={`bg-red z-30 w-full fixed top-0 py-4`}>
-      <div className="flex items-center justify-center mx-auto w-full">
+    <nav className={`bg-red z-40 w-full fixed top-0 py-4`}>
+      <div className={`${navBarIconClass} flex items-center mx-auto w-full`}>
         {/*Desktop Nav*/}
         {isAboveSmallScreens 
         ? 
@@ -53,10 +54,9 @@ const Navbar = ({ selectedPage, setSelectedPage}) => {
               setSelectedPage={setSelectedPage}
             />
           </div>)
-        
         : 
           (<button
-            className="rounded-full bg-red p-2"
+            className="rounded-full bg-red p-2 mr-[5%]"
             onClick={() => setIsMenuToggled(!isMenuToggled)}
           >
             <img alt="menu-icon" src="../assets/menu-icon.svg" />
@@ -64,10 +64,11 @@ const Navbar = ({ selectedPage, setSelectedPage}) => {
         }
         {/* Menu Popup */}
         {!isAboveSmallScreens && isMenuToggled && (
-          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+          <div className="fixed right-0 top-0 h-full bg-blue w-[300px]">
             {/*close icon */}
-            <div className="flex justify-end p-12">
-              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+            <div className="flex justify-end p-6">
+              <button
+                onClick={() => setIsMenuToggled(!isMenuToggled)}>
                 <img alt="close-icon" src="../assets/close-icon.svg"/>           
               </button>
             </div>
