21db4183580a89ff992aef01922093795aa8ec9e
Kevin Lee <jhssttj@hotmail.com>
2023-04-04 21:37:24 -0600

    Fix width reduction of text during xs and sm screen

--- src/scenes/Landing.js
+++ src/scenes/Landing.js
@@ -7,15 +7,15 @@ const Landing = ({ setSelectedPage }) => {
   const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
 
   return (
-    <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
+    <section id="home" className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10">
 
       {/*Image Section */}
-        <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32 border-2">
+        <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
           {isAboveMediumScreens
           ? (
           <div
             className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
-            before:w-full before:max-w-[400px] before:h-full before:border-4 before: border-blue before:z-[-1] before:rounded-r-xl"
+            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
           >
             <img
               alt="profile"
@@ -34,8 +34,7 @@ const Landing = ({ setSelectedPage }) => {
         </div>
 
         {/*Main Section */}
-      <div>
-        <div className="z-30 w-1/2 mt-12 md:mt-32">
+        <div className="z-30 basis-2/5 mt-12 md:mt-32">
           {/*Headings */}
           <motion.div
             initial="hidden"
@@ -56,9 +55,13 @@ const Landing = ({ setSelectedPage }) => {
             </p>
 
             <p className="mt-10 mb-7 text-sm text-center md:text-start">
-            Thank you for visiting my website! I am formerly a civil engineering graduate from University of Calgary who have recently took interest in web development.
+            Thank you for visiting my website! I am a former civil engineering graduate from University of Calgary who have taken interest in web development.
+            <br/>
+            <br/>
             Recently, I graduated from Lighthouse Labs Web Development Bootcamp where I learned how to utilize various programs and frameworks such as JavaScript, React, Ruby, etc. to create web based applications. With coding, I feel very rewarded for solving challenging problems using both creativity and logic. I also enjoy that there is always endless amount of content and skills to learn in this industry. 
             Through this learning, I discovered passion for coding and knew that this is what I wanted to do for the rest of my life. 
+            <br/>
+            <br/>
             Currently, I am seeking any web/software developing job opportunities as I look to integrate into the tech industry. 
             Please feel free to reach out to me for any potential openings or if you are interested in talking about coding and projects in general. 
             I would love to learn from other passionate developers!
@@ -78,24 +81,24 @@ const Landing = ({ setSelectedPage }) => {
              visible: {opacity: 1, x: 0}
            }}
           > 
-          <AnchorLink
-            className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold hover:bg-blue hover:text-white transition duration-500"
-            onClick={()=>setSelectedPage("contact")}
-            href="#contact"
-          >
-            Contact Me
-          </AnchorLink>
-          <AnchorLink
-            className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
-            onClick={()=>setSelectedPage("contact")}
-            href="#contact"
-          >
-            <div
-              className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center font-playfair px-10"
+            <AnchorLink
+              className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold hover:bg-blue hover:text-white transition duration-500"
+              onClick={()=>setSelectedPage("contact")}
+              href="#contact"
             >
-              Let's talk.
-            </div>
-          </AnchorLink>
+              Contact Me
+            </AnchorLink>
+            <AnchorLink
+              className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+              onClick={()=>setSelectedPage("contact")}
+              href="#contact"
+            >
+              <div
+                className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center font-playfair px-10"
+              >
+                Let's talk.
+              </div>
+            </AnchorLink>
           </motion.div>
 
           <motion.div
@@ -112,7 +115,6 @@ const Landing = ({ setSelectedPage }) => {
             <SocialMediaIcons/>
           </motion.div>
         </div>
-      </div>
     </section>
   )
 }
