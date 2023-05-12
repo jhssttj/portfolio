7116a1a2d9589bc436cfdd26d20d313ecba5ecb2
Kevin Lee <jhssttj@hotmail.com>
2023-05-11 22:01:20 -0600

    Work on fromatting landing page per media size

--- src/scenes/Landing.js
+++ src/scenes/Landing.js
@@ -5,14 +5,15 @@ import { BoxArrowDownRight } from 'react-bootstrap-icons';
 const Landing = () => {
 
   return (
-    <section id="home" className="md:h-screen sm:h-auto
-      py-14 bg-cover1 border-2 border-red flex flex-col justify-center
+    <section id="home" className="md:h-screen sm:h-screen xs:h-screen
+      md:bg-cover sm:bg-fill sm:bg-right-top
+      py-14 bg-cover4 flex flex-col justify-center
       ">
 
 
           {/*Headings */}
           <motion.div
-            className="relative top-[-100px] ml-[10%]"
+            className="relative top-[-100px] sm:ml-[10%] border-2 border-black text-center"
             initial="hidden"
             whileInView="visible"
             viewport={{once:true, amount:0.33}}
@@ -22,15 +23,15 @@ const Landing = () => {
               visible: {opacity: 1, x: 0}
             }}
           >
-            <p className="font-poppins text-6xl font-semibold">
+            <p className="font-poppins xs:text-6xl font-semibold">
               Hello there!
             </p>
-            <p className="font-poppins text-2xl mt-4 w-1/2">
+            <p className="font-poppins text-2xl mt-4">
               Thank you for visting my website. It would mean a lot if you could take the time to get to know me...
             </p>
           </motion.div>
           <motion.div
-            className="relative top-[-100px] flex justify-start mt-4 ml-[10%]" 
+            className="relative top-[-100px] flex justify-start mt-4 sm:ml-[10%]" 
             initial="hidden"
             whileInView="visible"
             viewport={{once:true, amount:0.33}}
@@ -41,8 +42,8 @@ const Landing = () => {
             }}
           >
             <AnchorLink
-              className="gap-2 flex border-2 border-black p-2 mt-4 hover:text-white 
-              hover:border-white hover:fill-white hover:fill-current animate-none hover:animate-bounce"
+              className="gap-2 flex border-2 border-black p-2 mt-4
+              hover:fill-white hover:fill-current animate-none hover:animate-bounce"
               href="#contact"
             >
               <p className="font-poppins text-2xl font-semibold">
