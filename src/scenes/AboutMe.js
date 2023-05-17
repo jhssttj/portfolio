7151ac02ff7e0b6f59ef7811e05d66099c1afe05
Kevin Lee <jhssttj@hotmail.com>
2023-05-16 23:45:18 -0600

    Finish styling format for small res

--- src/scenes/AboutMe.js
+++ src/scenes/AboutMe.js
@@ -5,14 +5,15 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const AboutMe = () => {
   
-  const buttonClass = "bg-blue text-white rounded-lg p-2 hover:bg-blueH shadow-xl"
+  const buttonClass = "bg-blue text-white rounded-lg p-2 hover:bg-blueH shadow-xl border-2 border-white"
 
   return (
     <section id="about" className="flex items-center justify-center min-h-screen">
 
         {/*Main Section */}
       <motion.div 
-        className="md:min-w-[1000px] md:w-[50%] h-1/2 md:flex shadow-2xl"
+        className="md:min-w-[1000px] md:w-[50%] 
+        h-1/2 md:flex-row flex flex-col items-center md:items-start shadow-2xl"
         initial="hidden"
         whileInView="visible"
         viewport={{once:true, amount:0.33}}
@@ -24,13 +25,13 @@ const AboutMe = () => {
       >
         {/* Left Side */}
         <div
-        className="w-[50%] bg-grey2 flex flex-col justify-between items-center
+        className="w-[100%] bg-grey2 flex flex-col justify-between items-center
         font-poppins text-center"
         >
           <div className="w-full h-[90%] flex flex-col justify-evenly items-center pt-8">
             <img
             alt="profile"
-            className="saturate-200 w-full max-w-[250px] rounded-full"
+            className="saturate-200 w-full max-w-[250px] rounded-full md:mt-4 mt-12"
             src="assets/profile-image.jpg"
             />
             <p className="text-4xl font-semibold p-8">
@@ -52,54 +53,54 @@ const AboutMe = () => {
               Full Stack Web Developer
             </p>
           </div>
-          <div className="w-full h-[10%] bg-white flex justify-center items-center">
+          <div className="w-full p-2 bg-white flex justify-center items-center">
             <SocialMediaIcons/>
           </div>
         </div>
 
        {/* Right Side  */}
         <div
-        className="bg-white w-[50%] p-4 flex flex-col justify-start"
+        className="bg-white w-[100%] p-4 flex flex-col md:text-start text-center"
         >
-        <h className="text-4xl font-semibold p-2 m-2">
-          About Me
-        </h>
-        <p className="text-gray-500 p-2 m-2">
-          In 2020, I graduated from University of Calgary with a Bachelor's in Civil Engineering and started working as a cost consultant.
-          I realised that this was not something I wanted to do for the rest of my life and started seeking for my true calling.
-          <br></br>
-          <br></br>
-          Recently, I graduated from Lighthouse Labs Web Development Bootcamp where I learned how to utilize various programs and frameworks such as JavaScript, React, Ruby, etc. to create 
-          web based applications. From this, I discovered passion for coding and knew that this is what I wanted to do for the rest of my life. 
-        </p>
-        {/* Anchor Links for Different Scenes */}
-        <div className="w-full p-2 m-2 flex justify-start gap-4">
-          <a
-            className={buttonClass}
-            href="https://resume.creddle.io/resume/11rq6q14mw6"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Resume
-          </a>
-          <AnchorLink
-            className={buttonClass}
-            href="#skills"
-          >
-            Skills
-          </AnchorLink>
-          <AnchorLink
-            className={buttonClass}
-            href="#projects"
-          >
-            Projects
-          </AnchorLink>
-          <AnchorLink
-            className={buttonClass}
-            href="#experiences"
-          >
-            Experiences
-          </AnchorLink>
+          <h className="text-4xl font-semibold p-2 m-2">
+            About Me
+          </h>
+          <p className="text-gray-500 p-2 m-2">
+            In 2020, I graduated from University of Calgary with a Bachelor's in Civil Engineering and started working as a cost consultant.
+            I realised that this was not something I wanted to do for the rest of my life and started seeking for my true calling.
+            <br></br>
+            <br></br>
+            Recently, I graduated from Lighthouse Labs Web Development Bootcamp where I learned how to utilize various programs and frameworks such as JavaScript, React, Ruby, etc. to create 
+            web based applications. From this, I discovered passion for coding and knew that this is what I wanted to do for the rest of my life. 
+          </p>
+          {/* Anchor Links for Different Scenes */}
+          <div className="w-full p-2 m-2 flex md:justify-start justify-center gap-4 xxs:grid-cols-2 grid text-center ">
+            <a
+              className={buttonClass}
+              href="https://resume.creddle.io/resume/11rq6q14mw6"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Resume
+            </a>
+            <AnchorLink
+              className={buttonClass}
+              href="#skills"
+            >
+              Skills
+            </AnchorLink>
+            <AnchorLink
+              className={buttonClass}
+              href="#projects"
+            >
+              Projects
+            </AnchorLink>
+            <AnchorLink
+              className={buttonClass}
+              href="#experiences"
+            >
+              Experiences
+            </AnchorLink>
         </div>
 
       
