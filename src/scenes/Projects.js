7498172677fd61f6df6d9f2fe9441278a7e0d418
Kevin Lee <jhssttj@hotmail.com>
2023-05-20 16:30:02 -0600

    Install react slide show to use for project slider

--- src/scenes/Projects.js
+++ src/scenes/Projects.js
@@ -1,105 +1,38 @@
 import {motion} from "framer-motion";
 import projectData from '../datas/projectData';
+import SmartSlider from "react-smart-slider";
 
 
-const container = {
-  hidden: {},
-  visible: {
-    transition: {staggerChildren: 0.2}
-  }
-}
 
-const projectVariant = {
-  hidden: { opacity: 0, scale: 0.8 },
-  visible: { opacity: 1, scale: 1}
-}
-
-const Project = ({title,description,link,git}) => {
-  const overlayStyles =`absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-yellow z-30 flex 
-  flex-col justify-center items-center text-center p-8 rounded-lg`
-  return (
-    <motion.div variants={projectVariant} className="relative m-2 border-black border-2 shadow-sd rounded-lg">
-      <div className={overlayStyles}>
-        <p className="font-poppins font-semibold text-sm xxs:text-2xl">{title}</p>
-        <p className="mt-2 text-xs xxs:text-base">
-          {description}
-        </p>
-        <a 
-        href={link} 
-        className="font-poppins text-black bg-blue hover:bg-purple font-semibold 
-        text-xs xxs:text-base px-4 py-2.5 mt-2 mr- mb-2 shadow-sd active:text-white"
-        rel="noopener noreferrer" target="_blank">
-          Project Demo
-        </a>
-        <a
-        className="hover:opacity-50 transition duration-500"
-        href={git}
-        target="_blank"
-        rel="noreferrer"
-        >
-        <img alt="projectgitlink" src="../assets/icons/github.png"/>
-        </a>
-      </div>
-      <div className="flex justify-center items-center">
-      <img className="p-1 "src={`../assets/projects/${title}.png`} alt={title} />
-      </div>
-    </motion.div>
-  )
-}
-
-const projectList = Object.values(projectData)
-const renderProjects = projectList.map((project) => {
-  return (<Project 
-    key={project.title}
-    title={project.title}
-    description={project.description}
-    link={project.link}
-    git={project.git}>
-    </Project>)
-})
 
 
 const Projects = () => {
-  return (
-    <section id="projects" className="pt-10 pb-24">
+  return(
+    <section id="projects" className="flex justify-center items-center min-h-screen">
+      {/*Content Section */}
       <motion.div
-      className="md:w-2/4 mx-auto text-center flex justify-center mt-16 mb-16"
-      initial="hidden"
-      whileInView="visible"
-      viewport={{once:true, amount:0.5}}
-      transition={{duration:0.5}}
-      variants={{
-        hidden: {opacity: 0, x: -50},
-        visible: {opacity: 1, x: 0}
-      }}
-      >
-          <p className="font-poppins font-semibold text-4xl flex justify-center md:justify-start">
-              <span className="relative z-20 before:content-brush3 before:absolute before:-top-[25px] before:z-[-1] 
-                before:-right-[40px]">
-                  MY
-                  <br></br>
-                  <span>
-                    PROJECTS
-                  </span>
-                </span>
-            </p>
-      </motion.div>
-
-      {/*Projects */}
-      <div className="flex justify-center">
-        <motion.div
-          className="sm:grid md:grid-cols-3 sm:grid-cols-2"
+          className="md:min-w-[1000px] md:w-[50%] h-[100%] 
+          h-3/4 shadow-xl flex flex-col bg-white md:m-24"
           initial="hidden"
           whileInView="visible"
           viewport={{once:true, amount:0.5}}
           transition={{duration:0.5}}
-          variants={container}
+          variants={{
+            hidden: {opacity: 0, x: -50},
+            visible: {opacity: 1, x: 0}
+          }}
         >
-          {/*Row 1 */}
-            {renderProjects}
-
-        </motion.div>
-      </div>
+        {/* Title Section */}
+        <div className="flex flex-col justify-start items-center bg-grey2 text-center h-auto">
+            <p className="text-4xl font-semibold p-2 md:m-2 mt-8">
+              Projects
+            </p>
+            <div className="border-b-4 border-blue w-[150px]"></div>
+            <p className="text-gray-500 p-2 m-2">
+              Check out some of my projects (More to come)!
+            </p>
+        </div>
+      </motion.div>
     </section>
   )
 }
--- src/scenes/test.js
+++ src/scenes/test.js
@@ -1,146 +1,107 @@
-import useMediaQuery from '../hooks/useMediaQuery';
-import skillData from '../datas/skillData';
 import {motion} from "framer-motion";
+import projectData from '../datas/projectData';
 
-const MySkills = () => {
-  const skillsClass = "flex justify-center items-start mt-2 grid mt-8 md:grid-cols-2 sm:grid-cols-5 xxs:grid-cols-3 grid-cols-2 bg-grey2 shadow-xl"
 
-  const renderSkills = (skillSet, color) => {
-    const skills = Object.values(skillData[skillSet]);
-    const render = skills.map((skill) => {
-      return (
-        <div key={skill.name} className="max-h-[100px] max-w-[100px] relative flex flex-col justify-start items-center rounded-lg border-2 border-black">
-            <div className={`absolute max-h-[75px] max-w-[75px] h-full w-full ${"bg-"+color} z-20 opacity-0 hover:opacity-100 transition duration-300 
-              flex justify-center rounded-lg shadow-xl`}>
-              <p className="font-poppins text-white text-sm xs:text-base text-center m-auto">
-                {skill.name}
-              </p>
-            </div>
-            <img key={skill.name} className="max-h-[75px] max-w-[75px] h-full w-full p-2"src={`../assets/skills/${skill.image}.png`} alt={skill.name} />
-        </div>
-      )
-      })
-    return render;
+const container = {
+  hidden: {},
+  visible: {
+    transition: {staggerChildren: 0.2}
   }
+}
+
+const projectVariant = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1}
+}
+
+const Project = ({title,description,link,git}) => {
+  const overlayStyles =`absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-yellow z-30 flex 
+  flex-col justify-center items-center text-center p-8 rounded-lg`
+  return (
+    <motion.div variants={projectVariant} className="relative m-2 border-black border-2 shadow-sd rounded-lg">
+      <div className={overlayStyles}>
+        <p className="font-poppins font-semibold text-sm xxs:text-2xl">{title}</p>
+        <p className="mt-2 text-xs xxs:text-base">
+          {description}
+        </p>
+        <a 
+        href={link} 
+        className="font-poppins text-black bg-blue hover:bg-purple font-semibold 
+        text-xs xxs:text-base px-4 py-2.5 mt-2 mr- mb-2 shadow-sd active:text-white"
+        rel="noopener noreferrer" target="_blank">
+          Project Demo
+        </a>
+        <a
+        className="hover:opacity-50 transition duration-500"
+        href={git}
+        target="_blank"
+        rel="noreferrer"
+        >
+        <img alt="projectgitlink" src="../assets/icons/github.png"/>
+        </a>
+      </div>
+      <div className="flex justify-center items-center">
+      <img className="p-1 "src={`../assets/projects/${title}.png`} alt={title} />
+      </div>
+    </motion.div>
+  )
+}
+
+const projectList = Object.values(projectData)
+const renderProjects = projectList.map((project) => {
+  return (<Project 
+    key={project.title}
+    title={project.title}
+    description={project.description}
+    link={project.link}
+    git={project.git}>
+    </Project>)
+})
 
 
-  return(
-    <section id="skills" className="pt-10 pb-24">
-      {/*Header and Image Section */}
+const Projects = () => {
+  return (
+    <section id="projects" className="pt-10 pb-24">
+      <motion.div
+      className="md:w-2/4 mx-auto text-center flex justify-center mt-16 mb-16"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{once:true, amount:0.5}}
+      transition={{duration:0.5}}
+      variants={{
+        hidden: {opacity: 0, x: -50},
+        visible: {opacity: 1, x: 0}
+      }}
+      >
+          <p className="font-poppins font-semibold text-4xl flex justify-center md:justify-start">
+              <span className="relative z-20 before:content-brush3 before:absolute before:-top-[25px] before:z-[-1] 
+                before:-right-[40px]">
+                  MY
+                  <br></br>
+                  <span>
+                    PROJECTS
+                  </span>
+                </span>
+            </p>
+      </motion.div>
+
+      {/*Projects */}
+      <div className="flex justify-center">
         <motion.div
-          className="w-full"
+          className="sm:grid md:grid-cols-3 sm:grid-cols-2"
           initial="hidden"
           whileInView="visible"
           viewport={{once:true, amount:0.5}}
           transition={{duration:0.5}}
-          variants={{
-            hidden: {opacity: 0, x: -50},
-            visible: {opacity: 1, x: 0}
-          }}
+          variants={container}
         >
-          {/* Title */}
-          <div className="border-2 border-red text-center">
-            <p className="font-semibold text-4xl">
-              Skills
-            </p>
-
-          </div>
-          {/* <p className="font-poppins font-semibold text-4xl mb-7 flex justify-center md:justify-start text-center md:text-start">
-            <span className="relative z-20 before:content-brush2 before:absolute before:-top-[50px] before:z-[-1] 
-              before:-right-[70px]">
-                MY
-                <br></br>
-                <span>
-                  SKILLS
-                </span>
-              </span>
-          </p>
-          <p className="mt-10 mb-7 flex flex-col justify-center md:flex-none text-lg">
-            I have many other skills and experiences related to:
-            <li>Project Management</li>
-            <li>Quality Control and Assurance</li>
-            <li>Engineering Softwares (AutoCAD, VisSim, CostX)</li>
-          </p> */}
-        </motion.div>
-
-      {/*Skills */}
-      <div className="md:flex md:justify-between mt-16 gap-32">
-        {/*Computer Lanaguage */}
-        <motion.div
-          className="md:w-1/3 mt-10 mb-10"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{once:true, amount:0.25}}
-          transition={{duration:0.5}}
-          variants={{
-            hidden: {opacity: 0, y: 50},
-            visible: {opacity: 1, y: 0}
-          }}
-        >
-          <div className="relative h-40 md:h-40 xxs:h-36 bg-purple shadow-sd rounded-lg border-2 border-black">
-            <div className="z-10 font-poppins">
-              <p className="font-semibold text-5xl ml-1">01</p>
-              <p className="font-semibold text-2xl mt-3 ml-1">Computer Languages</p>
-            </div>
-          </div>
-          <div className={skillsClass}>
-            {renderSkills("s1","purple")}
-          </div>
+          {/*Row 1 */}
+            {renderProjects}
 
         </motion.div>
-
-        {/*Framework, Libraries, Environments */}
-        <motion.div
-          className="md:w-1/3 mt-10 mb-10"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{once:true, amount:0.25}}
-          transition={{delay:0.3, duration:0.5}}
-          variants={{
-            hidden: {opacity: 0, y: 50},
-            visible: {opacity: 1, y: 0}
-          }}
-        >
-          <div className="relative h-40 md:h-40 xxs:h-36 bg-blue shadow-sd rounded-lg border-2 border-black">
-            <div className="z-10 font-poppins">
-              <p className="font-semibold text-5xl ml-1">02</p>
-              <p className="font-semibold text-2xl mt-3 ml-1">Framework, Libraries, Environments</p>
-            </div>
-          </div>
-          <div className={skillsClass}>
-            {renderSkills("s2", "blue")}
-          </div>
-
-        </motion.div>
-
-        {/*Testing, Systems, Datebases */}
-        <motion.div
-          className="md:w-1/3 mt-10 mb-10"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{once:true, amount:0.25}}
-          transition={{delay:0.6, duration:0.5}}
-          variants={{
-            hidden: {opacity: 0, y: 50},
-            visible: {opacity: 1, y: 0}
-          }}
-        >
-          <div className="relative h-40 md:h-40 xxs:h-36 bg-yellow shadow-sd rounded-lg border-2 border-black">
-            <div className="z-10 font-poppins">
-              <p className="font-semibold text-5xl ml-1">03</p>
-              <p className="font-semibold text-2xl mt-3 ml-1">Testing, Systems, Databases</p>
-            </div>
-          </div>
-          <div className={skillsClass}>
-            {renderSkills("s3", "yellow")}
-          </div>
-
-        </motion.div>
-
       </div>
-
     </section>
   )
 }
 
-export default MySkills;
+export default Projects;
